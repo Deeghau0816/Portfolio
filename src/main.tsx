f82b9d23e5ecdb09684c03c0d0a494ee55bafcd7
Deeghau Vidarshana <deeghauv110@gmail.com>
2025-10-21 02:15:15 +0530

    fix: clean up router setup - use only HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-﻿import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+﻿import './index.css';
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
-    <BrowserRouter basename="/Portfolio">
-      <HashRouter><App /></HashRouter>
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </ErrorBoundary>
 );
 
