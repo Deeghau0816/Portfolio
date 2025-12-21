566ddd6a0e05cfb54d4ed338529a09357239dd51
Deeghau Vidarshana <deeghauv110@gmail.com>
2025-12-21 10:22:27 +0530

    Fix router for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  // ✅ Required for GitHub Pages project sites
   base: "/Portfolio/",
-  server: {
-    open: true,
-  },
+  plugins: [react()],
 });
